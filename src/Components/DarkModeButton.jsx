6f89bd5637c9bc19d551f2b74ed59a72da2bbb26
VIN <v3p51435@gmail.com>
2024-08-26 16:59:43 +0530

    changed font

--- src/Components/DarkModeButton.jsx
+++ src/Components/DarkModeButton.jsx
@@ -17,7 +17,7 @@ const DarkModeToggle = () => {
         checked={!darkMode}
         onChange={() => setDarkMode(!darkMode)}
       />
-      <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-300">
+      <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-emerald-800 after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-emerald-800 after:border-zinc-700 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-300">
         <div className='flex justify-between items-center h-full'>
           <span className='text-black absolute left-1'><FiSun /></span>
           <span className='text-white absolute right-1'><FiMoon /></span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', 
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
